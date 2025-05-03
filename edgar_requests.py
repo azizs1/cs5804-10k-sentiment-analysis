4c6df1416fcb76b3215d1f3f70be4d44857fc203
Aziz Shaik <azizs@vt.edu>
2025-05-03 00:02:22 -0400

    Added more companies, added url for company_tickers.json

--- edgar_requests.py
+++ edgar_requests.py
@@ -7,19 +7,22 @@ from bs4 import BeautifulSoup
 
 # Header to use when sending requests to EDGAR
 headers = {
-    "User-Agent": "10KSentimentAnalysisProject/1.0 (azizs@vt.edu)"
+    'User-Agent': '10KSentimentAnalysisProject/1.0 (azizs@vt.edu)'
 }
 
-# Fortune 35 tickers
-tickers = ["wmt", "amzn", "aapl", "unh", "brka", "cvs", "xom",
-           "googl", "mck", "cor", "cost", "jpm", "msft", "cah",
-           "cvx", "ci", "f", "bac", "gm", "elv", "c", "cnc",
-           "hd", "mpc", "kr", "psx", "fnma", "wba", "vlo", "meta",
-           "vz", "t", "cmcsa", "wfc", "gs"]
+# Fortune 50 tickers
+tickers = ['wmt', 'amzn', 'aapl', 'unh', 'brka', 'cvs', 'xom',
+           'googl', 'mck', 'cor', 'cost', 'jpm', 'msft', 'cah',
+           'cvx', 'ci', 'f', 'bac', 'gm', 'elv', 'c', 'cnc',
+           'hd', 'mpc', 'kr', 'psx', 'fnma', 'wba', 'vlo', 'meta',
+           'vz', 't', 'cmcsa', 'wfc', 'gs', 'fmcc', 'tgt', 'hum',
+           'tsla', 'ms', 'jnj', 'adm', 'pep', 'ups', 'fdx', 'dis',
+           'dell', 'low', 'pg', 'et']
 
 # Use the SEC provided json to get CIKs
-with open('misc/company_tickers.json', 'r') as file:
-    company_tickers = json.load(file)
+url = 'https://www.sec.gov/files/company_tickers.json'
+response = requests.get(url, headers=headers)
+company_tickers = json.loads(response.text)
 
 # Store left-padded CIKs for each of our tickers as well as names
 companies = {}
@@ -31,27 +34,27 @@ for company in company_tickers.values():
 
 # Make requests to get all submissions for each company
 for ticker, company in companies.items():
-    url = f"https://data.sec.gov/submissions/CIK{company['cik']}.json"
+    url = f'https://data.sec.gov/submissions/CIK{company['cik']}.json'
     print(url)
     response = requests.get(url, headers=headers)
     
     submissions_dict = response.json()
-    dir_path = f"10k_reports/{company['name']}/"
+    dir_path = f'10k_reports/{company['name']}/'
 
     # Create directory path for this company if it does not exist
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     
     # Likely won't have to use this file, but saving it just in case
-    with open(dir_path+"submissions.json", 'w') as file:
+    with open(dir_path+'submissions.json', 'w') as file:
         json.dump(submissions_dict, file, indent=4)
 
     for idx, form in enumerate(submissions_dict['filings']['recent']['form']):
-        if form == "10-K":
+        if form == '10-K':
             accession_number = submissions_dict['filings']['recent']['accessionNumber'][idx]
             filename = submissions_dict['filings']['recent']['primaryDocument'][idx]
 
-            url = f"https://www.sec.gov/Archives/edgar/data/{company['cik']}/{accession_number.replace("-", "")}/{filename}"
+            url = f'https://www.sec.gov/Archives/edgar/data/{company['cik']}/{accession_number.replace('-', '')}/{filename}'
             response = requests.get(url, headers=headers)
             time.sleep(0.5)
 
@@ -60,18 +63,18 @@ for ticker, company in companies.items():
             print(file_10k)
 
             # Save the htm file locally
-            with open(dir_path+file_10k+'.htm', "wb") as file:
+            with open(dir_path+file_10k+'.htm', 'wb') as file:
                 file.write(response.content)
             time.sleep(0.5)
 
-            with open(dir_path+file_10k+'.htm', "r", encoding="utf-8") as file:
+            with open(dir_path+file_10k+'.htm', 'r', encoding='utf-8') as file:
                 html_content = file.read()
 
-            soup = BeautifulSoup(html_content, "html.parser")
+            soup = BeautifulSoup(html_content, 'html.parser')
 
             # Extract plain text
             plain_text = soup.get_text()
 
             # Save the plain text to a new file
-            with open(dir_path+file_10k+'.txt', "w", encoding="utf-8") as file:
+            with open(dir_path+file_10k+'.txt', 'w', encoding='utf-8') as file:
                 file.write(plain_text)
